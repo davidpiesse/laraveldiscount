52a29cf91177bb52304fce7230fd3d3f9cf5ab84
David Piesse <piesse@gmail.com>
2018-10-24 17:21:50 +0100

    WIP

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="w-full text-center my-2">    
-    <span class="no-underline text-grey-darker font-thin">Built by <a class="no-underline text-grey-darker font-thin" href="https://twitter.com/mapdev">David Piesse</a> using Laravel & TailwindCSS</span>
+    <span class="no-underline text-grey-darker font-thin">Built by <a class="no-underline text-grey-darker font-thin" href="https://twitter.com/mapdev">David Piesse</a> using <a class="no-underline text-grey-darker font-thin" href="https://laravel.com/">Laravel</a> & <a class="no-underline text-grey-darker font-thin" href="https://tailwindcss.com">TailwindCSS</a></span>
 </div>
