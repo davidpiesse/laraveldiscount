5c7904421eb0a8eb9edc9a6596fc50f02dc9a94b
David Piesse <piesse@gmail.com>
2018-10-24 17:19:13 +0100

    WIP

--- resources/views/components/boxOffer.blade.php
+++ resources/views/components/boxOffer.blade.php
@@ -1,10 +1,15 @@
-<a alt="{{ $offer->product->name}}" href="{{$offer->url}}" target="_blank" class="max-w-md w-full h-full lg:flex border border-grey-light rounded mb-2 bg-white no-underline hover:shadow-md">
-    <div class="h-16 w-16 p-2 flex items-center bg-cover text-center overflow-hidden" >
-        <img src="{{ $offer->product->logoUrl }}" alt="">
+<a alt="{{ $offer->product->name}}" href="{{$offer->url}}" target="_blank" class="max-w-md w-full h-full flex border border-grey-light rounded mb-2 bg-white no-underline hover:shadow-md">
+    <div class="flex items-center bg-cover overflow-hidden">
+        <div class="w-16 h-16 p-2">
+            <img src="{{ $offer->product->logoUrl }}" alt="">
+        </div>
     </div>
     <div class="p-2 flex flex-col justify-between leading-normal w-full">
         <div class="flex items-baseline justify-between">
-            <div class="text-black font-bold text-md">{{ $offer->title }} - <span class="text-base font-thin">{{ $offer->product->name }}</span></div>
+            <div class="block">
+                <div class="text-black font-bold text-md">{{ $offer->title }} </div>
+                <div class="text-grey-darker text-base font-thin">{{ $offer->product->name }}</div>
+            </div>
             @if($offer->hasDescription)
             <div class="text-grey text-xs text-right"> 
                 {{$offer->description}}
